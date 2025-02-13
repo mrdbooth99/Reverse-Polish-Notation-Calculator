930f6ac7a8c6a6a2f15a635a6569386fb696e9b3
mrdbooth99 <rdbooth@hotmail.com>
2025-02-13 19:53:06 +0000

    Task 3 - Hook up PolishNotationCalculator to Form1

--- RPN/Form1.cs
+++ RPN/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Drawing;
 using System.Windows.Forms;
 
 namespace RPN
@@ -12,11 +13,24 @@ namespace RPN
         public Form1()
         {
             InitializeComponent();
+            stack = new ArrayStack<double>(20);
+            calculator = new PolishNotationCalculator(stack);
         }
 
         private void Btn_Eval_Click(object sender, EventArgs e)
         {
-            // Read and Parse Expression here... 
+            try
+            {
+                Lbl_Output.Text = calculator.Evaluate(Txt_Input.Text).ToString();
+                Lbl_Output.ForeColor = Color.Black;
+            }
+            catch (Exception error){ 
+                    Lbl_Output.Text = error.Message;
+                    Lbl_Output.ForeColor = Color.Red;
+
+            }
+
+            
         }
     }
 }
